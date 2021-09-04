aeb771115639e0cf407e5fb94913d1d4b4efe126
mostafa <mostafaahmadi462@gmail.com>
2021-09-04 21:39:11 +0430

    update ServiceActivationDateModel dto

--- src/main/java/com/bontech/intershipt/demo/dto/ServiceActivationDateModel.java
+++ src/main/java/com/bontech/intershipt/demo/dto/ServiceActivationDateModel.java
@@ -1,9 +1,6 @@
 package com.bontech.intershipt.demo.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
@@ -11,6 +8,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class ServiceActivationDateModel {
 
     private LocalDate date;
