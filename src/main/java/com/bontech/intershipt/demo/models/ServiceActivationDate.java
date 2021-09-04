15c2aeeffbe7032ae80d032074e3549941ddcbc3
mostafa <mostafaahmadi462@gmail.com>
2021-09-04 21:38:38 +0430

    update some models

--- src/main/java/com/bontech/intershipt/demo/models/ServiceActivationDate.java
+++ src/main/java/com/bontech/intershipt/demo/models/ServiceActivationDate.java
@@ -1,9 +1,6 @@
 package com.bontech.intershipt.demo.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -13,6 +10,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @Entity
+@Builder
 public class ServiceActivationDate{
 
     @Id
