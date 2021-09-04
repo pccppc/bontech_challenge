c3f918bc1c5808c0c085e964459246e09cac0e73
mostafa <mostafaahmadi462@gmail.com>
2021-09-04 14:34:00 +0430

    update dto and add some feature

--- src/main/java/com/bontech/intershipt/demo/dto/UserModel.java
+++ src/main/java/com/bontech/intershipt/demo/dto/UserModel.java
@@ -1,5 +1,14 @@
 package com.bontech.intershipt.demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class UserModel {
 
     private String username;
